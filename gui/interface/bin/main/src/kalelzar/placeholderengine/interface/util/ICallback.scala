8e6b589ddfbc9f424de92733eb1167bd657926b4
Kalelzar <natomanofglory@gmail.com>
2019-09-20 18:48:16 +0300

    Fix the fix for the fix for ICallback

--- gui/interface/bin/main/src/kalelzar/placeholderengine/interface/util/ICallback.scala
+++ gui/interface/bin/main/src/kalelzar/placeholderengine/interface/util/ICallback.scala
@@ -2,34 +2,14 @@ package src.kalelzar.placeholderengine.interface.util
 
 import scala.collection.mutable
 
-trait ICallback {
-
-  private val callbacks = mutable.HashMap[String, Seq[Callback]]()
-
-  def registerCallback(name: String, f: Callback): Unit = {
-    println(f)
-    if (callbacks.contains(name)) callbacks(name) = callbacks(name) :+ f
-    else callbacks += ((name, Seq(f)))
-  }
-
-  def unregisterCallback(name: String, f: Callback): Unit = {
-    if (callbacks.contains(name) && callbacks(name).contains(f)) callbacks(name) = callbacks(name).filter(_ != f)
-  }
-
-  protected def callback(name: String, param: Any*): Unit = {
-    if (callbacks.contains(name)) callbacks(name).foreach(_(param))
-  }
-
-}
-
 trait Callback{
   def apply(p: Seq[Any]): Unit ={
     try{
       execute(p)
     }catch{
       //TODO: Add special handling of ClassCastException
-      case e: ClassCastException => throw RuntimeException(e)
-      case e => throw RuntimeException(e)
+      case e: ClassCastException => throw new RuntimeException(e)
+      case e => throw new RuntimeException(e)
     }
   }
   def execute(p: Seq[Any]): Unit
@@ -56,7 +36,7 @@ object Callback{
   implicit class F3Callback[A,B,C](f: (A, B, C) => Unit) extends Callback {
     override def execute(p: Seq[Any]): Unit = 
       f(p(0).asInstanceOf[A], 
-        p(1).asInstanceOf[B].
+        p(1).asInstanceOf[B],
         p(2).asInstanceOf[C])
   }
 }
